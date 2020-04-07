6fc74ac0a8332836957e5f85cefc76bb483623ba
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-04-07 12:57:32 -0700

    First Commit

---
+++ TraitCollectionDidChangeRepro/App.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+
+namespace TraitCollectionDidChangeRepro
+{
+    public partial class App : Xamarin.Forms.Application
+    {
+        public App()
+        {
+            var navigationPage = new Xamarin.Forms.NavigationPage(new MainPage());
+            navigationPage.On<iOS>().SetPrefersLargeTitles(true);
+
+            MainPage = navigationPage;
+        }
+    }
+}
---
+++ TraitCollectionDidChangeRepro/MainPage.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace TraitCollectionDidChangeRepro
+{
+    public class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            var collectionView = new CollectionView
+            {
+                ItemsSource = CreateItemSource(),
+            };
+
+            Content = new RefreshView
+            {
+                Content = collectionView
+            };
+        }
+
+        IEnumerable<string> CreateItemSource()
+        {
+            for (int i = 0; i < 100; i++)
+                yield return $"Row {i}";
+        }
+    }
+}
